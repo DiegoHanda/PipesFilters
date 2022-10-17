7fc5091f4b86bcd2f3c309dca540fa9822815ae5
Javier Odera <javier.odera@correo.ucu.edu.uy>
2022-10-17 20:42:56 -0300

    primer ejercicio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,17 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            IPipe Pipe3=new PipeNull();
+            IFilter Filtro2=new FilterNegative();
+
+            PipeSerial Pipe2=new PipeSerial(Filtro2, Pipe3);
+
+            IFilter Filtro1=new FilterGreyscale();
+
+            PipeSerial Pipe1=new PipeSerial(Filtro1,Pipe2);
         }
     }
 }
