2a4739f65536a0187ed0010d0fb21c640f6a743d
braimor <briangmsosa@gmail.com>
2022-10-19 18:50:19 -0300

    Ejercicio 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,20 +9,22 @@ namespace CompAndDel
         static void Main(string[] args)//Hay que hacer una nueva clase filtro que lo unico que haga es guardar la imagen despues de un paso, e implementarla despues de cada filtro
         {
             PictureProvider provider = new PictureProvider();
+
             IPicture picture = provider.GetPicture(@"luke.jpg");
-
             IPipe Pipe3=new PipeNull();
-            IFilter Filtro2=new FilterNegative();
+            IPicture image3=Pipe3.Send(picture);
 
+            IFilter Filtro2=new FilterNegative();
             PipeSerial Pipe2=new PipeSerial(Filtro2, Pipe3);
+            IPicture image2=Pipe2.Send(picture);
 
             IFilter Filtro1=new FilterGreyscale();
-
             PipeSerial Pipe1=new PipeSerial(Filtro1,Pipe2);
-
             IPicture image=Pipe1.Send(picture);
 
             provider.SavePicture(image, @"Imagen_Editada.jpg");
+            provider.SavePicture(image2, @"Imagen_Editada2.jpg");
+            provider.SavePicture(image3, @"Imagen_Editada3.jpg");
         }
     }
 }
