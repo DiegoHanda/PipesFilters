79508fa231d1eef4c665b3ae7754ae7b54c6d494
Javier Odera <javier.odera@correo.ucu.edu.uy>
2022-10-23 15:20:49 -0300

    ejercicio 2 finalizado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,21 +12,15 @@ namespace CompAndDel
 
             IPicture picture = provider.GetPicture(@"luke.jpg");
             IPipe final=new PipeNull();
-            IPicture image3=final.Send(picture);
-
-            IPipe Pipe5
 
             IFilter FiltroSave2=new FilterSave(2);
-            IPipe Pipe5 = new PipeSerial(FiltroSave2,Pipe6);
+            IPipe Pipe4 = new PipeSerial(FiltroSave2,final);
             
-            IFilter FiltroNG=new FilterNegative();
-            IPipe Pipe4= new PipeSerial(FiltroNG, Pipe5);
+            IFilter FiltroNG=new FilterNegative();    
+            IPipe Pipe3 = new PipeSerial(FiltroNG, Pipe4);
 
-            IFilter FiltroSave=new FilterSave(1);
-            IPipe Pipe3 = new PipeSerial(FiltroSave, Pipe4);
-
-            IFilter FiltroGS= new FilterGreyscale();
-            PipeSerial Pipe2=new PipeSerial(FiltroGS, Pipe3);
+            IFilter FiltroSave= new FilterSave(1);
+            PipeSerial Pipe2=new PipeSerial(FiltroSave, Pipe3);
 
             IFilter Filtro1=new FilterGreyscale();
             PipeSerial Pipe1=new PipeSerial(Filtro1,Pipe2);
